3bc2e8065247d2d1c148639b749bf1f0fdfeebae
saw <sawrov@hotmail.com>
2019-06-05 19:41:07 -0700

    test

--- testrules.py
+++ testrules.py
@@ -1,37 +1,64 @@
 from Preprocessing import process
 
-load_test_data=process(source="KDDTest+.txt")
-load_test_data.extract_info()
+test=process(source="KDDTest+.txt")
+test.extract_info()
+
+def predict(individual, specimen):
+
+        w1=0.239
+        w2=0.181
+        w3=0.215
+        w4=0.162
+        w5=0.069
+        w6=0.064
+        w7=0.055
+        w8=0.015
+        outcome=0
+        if individual[0:7]==specimen[0:7]:
+            outcome+=w1
+        if individual[7:17]==specimen[7:17]:
+            outcome+=w2
+        if individual[17:29]==specimen[17:29]:
+            outcome+=w3
+        if individual[29:31]==specimen[29:31]:
+            outcome+=w4
+        if individual[31:35]==specimen[31:35]:
+            outcome+=w5
+        if individual[35:45]==specimen[35:45]:
+            outcome+=w6
+        if individual[45:52]==specimen[45:52]:
+            outcome+=w7
+        if individual[52:53]==specimen[52:53]:
+            outcome+=w8
+        return outcome
 
 file="Results/Best_Results.txt"
 with open("Results/recent.txt") as f:
     file=str(f.readline())
-test_data=[]
-print(len(load_test_data.genotype[1]))
-print(load_test_data.genotype[1])
+rules=[]
 with open(file) as fp:
     line=fp.readline()
     while line:
             data=line.split('\n')[0].split(' ')
-            test_data.append(map(int,data))
+            rules.append(map(int,data))
             line=fp.readline()
 
-
-this=(len(test_data[0])-1)
-print(load_test_data.genotype[1][this])
-
 def isanattack(individual):
-    global test_data
-    for data in test_data:
-        flag = 1
-        for i in range(0,len(data)):
-		    if(str(data[i])!= str(individual[i])):
-			    flag=0
-        if(flag==1):
-            return flag
-    return flag
+    global rules
+    for rule in rules:
+        outcome=predict(individual,rule)
+        if(outcome>0.5):
+            if(rule[-1]==1):
+                print("Attack")
+            else:
+                print("notanattack")
+            return
+        
 
-for data in load_test_data.genotype:
-    pred=isanattack(data)
-    print("pred="+str(pred))
+for data in test.genotype:
+    print(data)
+    print(data[-2])
+    isanattack(data)
+    print("------------------")
+
     # print("Actual="+str(data[len(test_data[0])]))
